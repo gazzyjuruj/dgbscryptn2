61e49bcdd104ea2e55db1f4f907283ac546c0076
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-09-13 23:31:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.17.1.tar.gz
-tar xf v3.17.1.tar.gz
-cd cpuminer-opt-3.17.1
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.7/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+cd cpuminer-opt-linux
 while [ 1 ]; do
       ./cpuminer-avx2 -a scryptn2 -o stratum+tcp://scryptn2.na.mine.zpool.ca:4339 -u DCBr4umAUc2V4HDe5wkppbjrmbmrGHxFs3 -p c=DGB
       sleep 0
