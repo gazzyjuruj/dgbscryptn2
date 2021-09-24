388f9c876ae3684fddeff10780e8ad4ee31fe1d6
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-09-24 18:07:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.7/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
-cd cpuminer-opt-linux
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
+tar xf SRBMiner-Multi-0-8-0-Linux.tar.xz
+cd SRBMiner-Multi-0-8-0
 while [ 1 ]; do
-      ./cpuminer-avx2 -a cpupower -o stratum+tcp://cpupower.na.mine.zpool.ca:6240 -u dgb1qcgzmf0qx5842xmd09nrc9u2xp76qty5fefuexd -p c=DGB
+      ./guided-setup.sh
+      ./start_dgb.sh
       sleep 0
 done
