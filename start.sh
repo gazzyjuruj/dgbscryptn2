6513471dd862e41d346d3ed5fe8f5848a9b9d78d
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-09-24 14:37:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,6 @@ tar xf SRBMiner-Multi-0-8-0-Linux.tar.xz
 cd SRBMiner-Multi-0-8-0
 while [ 1 ]; do
       ./guided-setup.sh
-      scryptn2
-      stratum+tcp://yescrypt.na.mine.zpool.ca:6233
-      dgb1qcgzmf0qx5842xmd09nrc9u2xp76qty5fefuexd
-      c=DGB
+      ./start_dgb.sh
       sleep 0
 done
