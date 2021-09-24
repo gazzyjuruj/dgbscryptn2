150680c9ecb77756ed5e2572bb761035f35d8cf8
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-09-24 14:30:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
 tar xf SRBMiner-Multi-0-8-0-Linux.tar.xz
-cd cpuminer-opt-linux
+cd SRBMiner-Multi-0-8-0
 while [ 1 ]; do
-      ./cpuminer-avx2 -a yescrypt -o stratum+tcp://yescrypt.na.mine.zpool.ca:6233 -u dgb1qcgzmf0qx5842xmd09nrc9u2xp76qty5fefuexd -p c=DGB
+      ./guided-setup.sh
+      scryptn2
+      stratum+tcp://yescrypt.na.mine.zpool.ca:6233
+      dgb1qcgzmf0qx5842xmd09nrc9u2xp76qty5fefuexd
+      c=DGB
       sleep 0
 done
