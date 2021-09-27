5afe8a58f90cdda467835a907603d4faf15e9aac
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-09-28 00:49:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ tar xf cpuminer-opt-linux.tar.gz
 cd cpuminer-opt-linux
 while [ 1 ]; do
       #./cpuminer-avx2 -a scryptn2 -o stratum+tcp://scryptn2.na.mine.zpool.ca:4339 -u dgb1qqrpgpansa7ktrvphwqwwgr8g62c9fc8xa8dhqd -p c=DGB
-      ./cpuminer-avx2 -a scryptn2 -o stratum+tcp://scryptn2.asia.mine.zergpool.com:3435 -u dgb1qqrpgpansa7ktrvphwqwwgr8g62c9fc8xa8dhqd -p c=DGB
+      ./cpuminer-avx2 -a scryptn2 -o stratum+tcp://scryptn2.eu.mine.zergpool.com:3435 -u dgb1qqrpgpansa7ktrvphwqwwgr8g62c9fc8xa8dhqd -p c=DGB
       sleep 0
 done
